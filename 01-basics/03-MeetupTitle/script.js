584db0f7aed8a4afa721131fb6fd951277f7f071
UnderCore <undercore999@yandex.ru>
2021-06-08 16:47:59 +0300

    add solution for meetup title

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,31 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// Требуется создать Vue приложение
+const app = new Vue({
+	data (){
+		return {
+			selectedItem: null,
+			title: null,
+			isLoading: false,
+		};
+	},
+	methods: {
+		loadMeetup(id){
+			this.isLoading = true
+			fetch('https://course-vue.javascript.ru/api/meetups/' + id)
+				.then(response => response.json())
+				.then(jsonResponse => {
+					this.isLoading = false
+					this.title = jsonResponse.title
+				})
+		},
+	},
+	watch: {
+		selectedItem(val, old){
+			if(val && val != old){
+				this.loadMeetup(val)
+			}
+		}
+	},
+});
+
+app.$mount('#app');
