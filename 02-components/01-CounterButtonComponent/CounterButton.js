9c6f09d1609d1d0eda9128bb189143a8d5cac4c3
UnderCore <undercore999@yandex.ru>
2021-06-14 21:19:56 +0300

    add solution for 02 components - 01 counter button

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,14 +1,29 @@
-const CounterButton = {
+export default {
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
 
   // Компонент должен иметь модель
+  data() {
+    return {
+      counter: 0,
+    };
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    increment() {
+      this.counter = this.count;
+      this.$emit('increment', ++this.counter);
+    },
+  },
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button"></button>`,
+  template: '<button type="button" @click="increment">{{ count }}</button>',
 };
-
-export default CounterButton;
