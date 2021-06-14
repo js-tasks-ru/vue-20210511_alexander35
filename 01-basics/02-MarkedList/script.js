20b59de4504d02c3eb863293d85a983821649ef6
UnderCore <undercore999@yandex.ru>
2021-06-14 21:04:49 +0300

    fix: solution for 01 basics - 02 marked list

--- 01-basics/02-MarkedList/script.js
+++ 01-basics/02-MarkedList/script.js
@@ -3,58 +3,51 @@ import Vue from './vendor/vue.esm.browser.js';
 // From https://jsonplaceholder.typicode.com/comments
 
 const emails = [
-	{value: 'Eliseo@gardner.biz', marked: false},
-	{value: 'Jayne_Kuhic@sydney.com', marked: false},
-	{value: 'Nikita@garfield.biz', marked: false},
-	{value: 'Lew@alysha.tv', marked: false},
-	{value: 'Hayden@althea.biz', marked: false},
-	{value: 'Presley.Mueller@myrl.com', marked: false},
-	{value: 'Dallas@ole.me', marked: false},
-	{value: 'Mallory_Kunze@marie.org', marked: false},
-	{value: 'Meghan_Littel@rene.us', marked: false},
-	{value: 'Carmen_Keeling@caroline.name', marked: false},
-	{value: 'Veronica_Goodwin@timmothy.net', marked: false},
-	{value: 'Oswald.Vandervort@leanne.org', marked: false},
-	{value: 'Kariane@jadyn.tv', marked: false},
-	{value: 'Nathan@solon.io', marked: false},
-	{value: 'Maynard.Hodkiewicz@roberta.com', marked: false},
-	{value: 'Christine@ayana.info', marked: false},
-	{value: 'Preston_Hudson@blaise.tv', marked: false},
-	{value: 'Vincenza_Klocko@albertha.name', marked: false},
-	{value: 'Madelynn.Gorczany@darion.biz', marked: false},
-	{value: 'Mariana_Orn@preston.org', marked: false},
-	{value: 'Noemie@marques.me', marked: false},
-	{value: 'Khalil@emile.co.uk', marked: false},
-	{value: 'Sophia@arianna.co.uk', marked: false},
-	{value: 'Jeffery@juwan.us', marked: false},
-	{value: 'Isaias_Kuhic@jarrett.net', marked: false}
+	'Eliseo@gardner.biz',
+	'Jayne_Kuhic@sydney.com',
+	'Nikita@garfield.biz',
+	'Lew@alysha.tv',
+	'Hayden@althea.biz',
+	'Presley.Mueller@myrl.com',
+	'Dallas@ole.me',
+	'Mallory_Kunze@marie.org',
+	'Meghan_Littel@rene.us',
+	'Carmen_Keeling@caroline.name',
+	'Veronica_Goodwin@timmothy.net',
+	'Oswald.Vandervort@leanne.org',
+	'Kariane@jadyn.tv',
+	'Nathan@solon.io',
+	'Maynard.Hodkiewicz@roberta.com',
+	'Christine@ayana.info',
+	'Preston_Hudson@blaise.tv',
+	'Vincenza_Klocko@albertha.name',
+	'Madelynn.Gorczany@darion.biz',
+	'Mariana_Orn@preston.org',
+	'Noemie@marques.me',
+	'Khalil@emile.co.uk',
+	'Sophia@arianna.co.uk',
+	'Jeffery@juwan.us',
+	'Isaias_Kuhic@jarrett.net'
 ];
 
 const app = new Vue({
 	data (){
 		return {
-			emails,
 			emailModel: '',
 		};
 	},
-	watch: {
-		emailModel(val, old){
-			this.updateEmails(val)
-		}
-	},
-	methods: {
-		updateEmails() {
-			return emails.forEach(function (item, key) {
-				if (app.emailModel == 0) {
-					item.marked = false
-				} else {
-					item.marked = (item.value.toLowerCase().indexOf(app.emailModel.toLowerCase()) != -1) ? true : false
+	computed: {
+		getMarkedEmails() {
+			return emails.map(item => {
+				let marked = false
+				if (app.emailModel != 0) {
+					marked = (item.toLowerCase().indexOf(app.emailModel.toLowerCase()) != -1) ? true : false
 				}
-			});
-		}
+				item = { value: item, marked }
+				return item
+			})
+		},
 	}
 });
 
 app.$mount('#app');
-
-// Требуется создать Vue приложение
